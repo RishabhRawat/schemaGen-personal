145a0ded0d3ef9db440fe90a76ece65713cbc424
Rishabh Rawat <rishabhrawat1994@gmail.com>
2016-03-05 01:21:38 +0530

    Using jointjs

--- schematic.js
+++ schematic.js
@@ -1,21 +1,66 @@
-var s = Snap('#schema_surface');
-s.width = document.getElementById('schematic_container').offsetWidth;
-s.height = document.getElementById('schematic_container').offsetHeight;
-s.attr({color:'#00FF00'});
-
-var hr = s.line(s.width*0.9,0, s.width*0.9, s.height);
-hr.attr({stroke:'#000000'});
-Snap.load("gates.svg", function (f) {
-    g = f.select("#andgate");
-    s.append(g);
-    // Making croc draggable. Go ahead drag it around!
-    g.drag();
-    g.click(function(){
-        alert("correct");
-    });
-    // Obviously drag could take event handlers too
-    // That’s better! selectAll for the rescue.
+var graph = new joint.dia.Graph;
+var paper = new joint.dia.Paper({
+    el: $('#schematic_container'),
+    width: '100%',
+    height: '100%',
+    model: graph,
+    gridSize: 1
 });
-var c = s.getBBox();
+
+joint.shapes.basic.AndGate = joint.shapes.basic.Generic.extend({
+    markup: '<g class="component">' +
+    '<path d="M0 0 h35 a25 25 0 1 1 0 50  h-35  Z" transform="translate(20 0)"/>' +
+    '<path d="M0 0 h20" transform="translate(0 15)"/>' +
+    '<path d="M0 0 h20" transform="translate(0 35)"/>' +
+    '<path d="M0 0 h-20" transform="translate(100 25)"/>' +
+    '</g>'
+});
+
+joint.shapes.basic.OrGate = joint.shapes.basic.Generic.extend({
+    markup: '<g class="component">' +
+    '<path d="M0 0 Q40 0 60 25 Q40 50 0 50 q20 -25 0 -50 M0 15 l8 0 M0 35 l8 0" transform="translate(20 0)"/>' +
+    '<path d="M0 0 h20" transform="translate(0 15)"/>' +
+    '<path d="M0 0 h20" transform="translate(0 35)"/>' +
+    '<path d="M0 0 h-20" transform="translate(100 25)"/>' +
+    '</g>'
+});
+
+joint.shapes.basic.NotGate = joint.shapes.basic.Generic.extend({
+    markup: '<g class="component">' +
+    '<path d="M0 0 L50 25 a5 5 0 1 0 10 0 a5 5 0 1 0 -10 0 L0 50 Z" transform="translate(20 0)"/>' +
+    '<path d="M0 0 h20" transform="translate(0 25)"/>' +
+    '<path d="M0 0 h-20" transform="translate(100 25)"/>' +
+    '</g>'
+});
+
+joint.shapes.basic.InputGate = joint.shapes.basic.Generic.extend({
+    markup: '<g class="component">' +
+    '<path d="M0 15 h40 L60 25 L40 35 h-40 Z" transform="translate(20 0)"/>' +
+    '<path d="M0 0 h-20" transform="translate(100 25)"/>' +
+    '</g>'
+});
+
+joint.shapes.basic.OutputGate = joint.shapes.basic.Generic.extend({
+    markup: '<g class="component">' +
+    '<path d="M0 25 L20 15 h40 v20 h-40 Z" transform="translate(20 0)"/>' +
+    '<path d="M0 0 h20" transform="translate(0 25)"/>' +
+    '</g>'
+});
 
 
+var rect = new joint.shapes.basic.InputGate({
+    position: {x: 100, y: 30},
+});
+
+var rect2 = new joint.shapes.basic.OutputGate({
+    position: {x: 100, y: 30},
+});
+rect2.translate(300);
+
+var link = new joint.dia.Link({
+    source: {id: rect.id},
+    target: {id: rect2.id}
+});
+
+
+graph.addCells([rect, rect2, link]);
